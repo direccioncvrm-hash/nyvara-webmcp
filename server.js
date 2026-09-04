0979e4b563c9911a3145113a55399013a05bac75
direccioncvrm-hash <direccion.cvrm@gmail.com>
2026-09-04 09:11:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-
+app.use(express.static(__dirname));
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
